6f551a83bf4770b74e284d42bf4f6753ea98ac31
Zeph Levy <171337931+ZephLevy@users.noreply.github.com>
2026-01-13 16:29:44 +0100

    Add a placeholder frontend for now

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>No frontend yet!</h1>
+
+<p>If you know your way around GraphQL, you can try <a href="http://localhost:3000/graphiql">directly interfacing
+with the backend.</a></p>
