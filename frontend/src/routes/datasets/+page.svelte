c89f2f1c1989c1f49d9f78c898cdfae65492fa1e
Zeph Levy <171337931+ZephLevy@users.noreply.github.com>
2026-01-14 12:11:42 +0100

    Add a check to the datasets page to not display an empty page if there are no datasets

--- frontend/src/routes/datasets/+page.svelte
+++ frontend/src/routes/datasets/+page.svelte
@@ -4,11 +4,23 @@
   let { data }: PageProps = $props();
 </script>
 
-<ul>
+<svelte:head>
+  <title>List of datasets</title>
+</svelte:head>
+
+{#if data.datasets.length}
   {#each data.datasets as dataset}
-    <li>
-      <strong>{dataset.name}</strong><br />
-      {dataset.description}
-    </li>
+    <nav>
+      <ul>
+        <li>
+          <a href="datasets/{dataset.id}"><strong>{dataset.name}</strong></a><br
+          />
+          {dataset.description}
+        </li>
+      </ul>
+    </nav>
   {/each}
-</ul>
+{:else}
+  <h1>No datasets yet!</h1>
+  <p>Try adding a dataset through the backend.</p>
+{/if}
